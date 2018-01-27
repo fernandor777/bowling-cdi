03e3169702d24bc35ab85136bb922b8ea33094bf
Fernando Miño <fernandomino7@gmail.com>
2018-01-27 12:01:50 -0500

    fix

--- src/main/java/org/fmino/bowlingscore/impl/FrameFacadeImpl.java
+++ src/main/java/org/fmino/bowlingscore/impl/FrameFacadeImpl.java
@@ -1,9 +1,21 @@
 package org.fmino.bowlingscore.impl;
 
-import org.fmino.bowlingscore.api.FrameFacade;
+import java.util.List;
 
+import javax.enterprise.context.Dependent;
+
+import org.fmino.bowlingscore.api.FrameFacade;
+import org.fmino.bowlingscore.model.Pinfall;
+import org.fmino.bowlingscore.model.PlayerFrame;
+
+@Dependent
 public class FrameFacadeImpl implements FrameFacade {
 	
-	
+	public PlayerFrame getFrame(List<Pinfall> pfs, int number){
+		PlayerFrame frame = new PlayerFrame(number);
+		frame.setPinfalls(pfs);
+		
+		return frame;
+	}
 	
 }
